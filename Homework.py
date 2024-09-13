0dfcefe920a8833977936c1c3360fd18604f9591
Mohammedoof <119490011+Mohammedoof@users.noreply.github.com>
2024-09-13 17:45:02 +0300

    Update Homework.py
    I add the homework of Breath-First Search

--- Homework.py
+++ Homework.py
@@ -1,1 +1,26 @@
-print("hello world")
+graph = {
+    '5':['3','7'],
+    '3':['2','4'],
+    '7':['8'],
+    '2':[],
+    '4':['8'],
+    '8':[]
+}
+visited = [] 
+queue = [] 
+def bfs(visited, graph, root,des):
+    visited.append(root)
+    queue.append(root)
+    while queue:
+        m=queue.pop(0)
+        print(m,end=(" "))
+        if m == des :
+            break
+        for n in graph[m]:
+            if n  not in visited:
+                visited.append(n)
+                queue.append(n)
+
+
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '5', '7') 
